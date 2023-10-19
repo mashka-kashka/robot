def42e6622ab0d1943a03aad3bee918c7866d311
Maria Muravieva <maria.muravieva@mail.ru>
2023-10-19 20:44:36 +0300

    Улучшено оформление кода

--- robot.py
+++ robot.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import cv2
 import os
 
+
 # Параметры кадра
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
@@ -47,61 +48,50 @@ vert_search_step = VERT_STEP
 faceview_time = datetime.now()
 say_hello = True
 
-while True:
-    frame = picam2.capture_array()
-
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(grey, 1.1, 5)
-    
+# Отображение температуры процессора
+def print_cpu_temperature():
+    # Настраиваем цвет в зависимости от температуры процессора
     text_color =  (0, 200, 0)
     if (cpu.temperature > 60):
         text_color = (0, 200, 200)
-    if (cpu.temperature > 70):
+    if (cpu.temperature > 80):
         text_color = (0, 0, 200)
         
     cv2.putText(frame, f'Температура процессора: {int(cpu.temperature)}', 
         (10, 20), font, 0.5, text_color, 1, cv2.LINE_AA)
-   
-    if len(faces)==0: # Лица не найдены
-        time_delta = datetime.now() - faceview_time
-        if time_delta.total_seconds() < SEARCH_DELAY:
-            cv2.putText(frame, f'Лица не найдены {int(time_delta.total_seconds())} секунд', 
-                (10, 40), font, 0.5, text_color, 1, cv2.LINE_AA)          
-        else:
-            say_hello = True
-            cv2.putText(frame, 'Ищу лица', 
-                (10, 40), font, 0.5, text_color, 1, cv2.LINE_AA)
 
-            hor_position = hor_position + hor_search_step
-            if hor_search_step > 0 and hor_position > MAX_HOR_ANGLE:
-                hor_position = MAX_HOR_ANGLE
-                hor_search_step = hor_search_step * -1
-            elif hor_search_step < 0 and hor_position < MIN_HOR_ANGLE:
-                hor_position = MIN_HOR_ANGLE
-                hor_search_step = hor_search_step * -1
-                # Поиск по вертикали
-                vert_position = vert_position + vert_search_step
-                if vert_search_step > 0 and vert_position > MAX_VERT_ANGLE:
-                    vert_position = MAX_VERT_ANGLE
-                    vert_search_step = vert_search_step * -1
-                elif vert_search_step < 0 and vert_position < MIN_VERT_ANGLE:
-                    vert_position = MIN_VERT_ANGLE
-                    vert_search_step = vert_search_step * -1
-                pwm.setServoPwm('1', vert_position)
-            pwm.setServoPwm('0', hor_position)
-    else:
-        if say_hello:
-            os.system('aplay ./audio/привет.wav')
-            say_hello = False
-        faceview_time = datetime.now()
+# Поиск лиц
+def search_faces(): 
+    # Поиск по горизонтали
+    hor_position = hor_position + hor_search_step
+    if hor_search_step > 0 and hor_position > MAX_HOR_ANGLE:
+        hor_position = MAX_HOR_ANGLE
+        hor_search_step = hor_search_step * -1
+    elif hor_search_step < 0 and hor_position < MIN_HOR_ANGLE:
+        hor_position = MIN_HOR_ANGLE
+        hor_search_step = hor_search_step * -1
+    pwm.setServoPwm('0', hor_position)
+    
+    # Поиск по вертикали
+    vert_position = vert_position + vert_search_step
+    if vert_search_step > 0 and vert_position > MAX_VERT_ANGLE:
+        vert_position = MAX_VERT_ANGLE
+        vert_search_step = vert_search_step * -1
+    elif vert_search_step < 0 and vert_position < MIN_VERT_ANGLE:
+        vert_position = MIN_VERT_ANGLE
+        vert_search_step = vert_search_step * -1
+    pwm.setServoPwm('1', vert_position)
 
+# Отслеживание лиц
+def track_faces(faces):
     left = FRAME_WIDTH
     right = 0
     top = 0
     bottom = FRAME_HEIGHT
     for (x, y, w, h) in faces:
+        # Отображение рамок
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0))
-
+    
         if x < left:
             left = x
         if x + w > right:
@@ -135,6 +125,36 @@ while True:
         if (vert_position < MIN_VERT_ANGLE):
             vert_position = MIN_VERT_ANGLE
         pwm.setServoPwm('1', vert_position)
+
+while True:
+    frame = picam2.capture_array()
+    # Отображение температуры процессора
+    print_cpu_temperature()
+
+    # Обнаружение лиц в кадре
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(grey, 1.1, 5)
+    if len(faces)==0: # Лица не обнаружены
+        time_diff = datetime.now() - faceview_time
+        delay = int(time_diff.total_seconds())
+        if delay < SEARCH_DELAY:
+            cv2.putText(frame, 
+                f'Лица не найдены {delay} секунд', 
+                (10, 40), font, 0.5, text_color, 1, cv2.LINE_AA)          
+        else:
+            say_hello = True
+            cv2.putText(frame, 'Ищу лица', 
+                (10, 40), font, 0.5, text_color, 1, cv2.LINE_AA)
+            search_faces()
+    else: # Лица обнаружены
+        faceview_time = datetime.now()
+        
+        if say_hello:
+            os.system('aplay ./audio/привет.wav')
+            say_hello = False
+        
+        # Ценрирование камеры на лицах
+        track_faces(faces)
 
     # Отображение кадра
     cv2.imshow('Робот', frame)
