a603018dd915daef8b6ae7fcb54b667fcb47702d
Sergey Muravev <smurav@mail.ru>
2024-11-05 21:45:12 +0300

    В модель распознавания эмоций переведена только на особые точки

--- train/emotions_model.py
+++ train/emotions_model.py
@@ -9,15 +9,16 @@ class EmotionsNet(nn.Module):
             super().__init__()
             self.dim = len(labels)
             self.labels = labels
-            self.fc1 = nn.Linear(FACEMESH_NUM_LANDMARKS_WITH_IRISES * 3 + 52, 1024).double()
-            self.fc2 = nn.Linear(1024, 512).double()
-            self.fc3 = nn.Linear(512, 256).double()
-            self.fc4 = nn.Linear(256, self.dim).double()
+            #self.fc1 = nn.Linear(FACEMESH_NUM_LANDMARKS_WITH_IRISES * 3 + 52, 1024).double()
+            self.fc1 = nn.Linear(52, 24).double()
+            self.fc2 = nn.Linear(24, 12).double()
+#            self.fc3 = nn.Linear(512, 256).double()
+            self.fc4 = nn.Linear(12, self.dim).double()
 
         def forward(self, x):
             x = F.relu(self.fc1(x))
             x = F.relu(self.fc2(x))
-            x = F.relu(self.fc3(x))
+            #x = F.relu(self.fc3(x))
             x = F.softmax(self.fc4(x), dim=1)
             return x
 
